5efd5cc7e9bb315f21ee51f393cc6d657b13633a
jlgy <trustour9@gmail.com>
2025-02-06 13:55:25 +0800

    add healthBar

--- src/js/modules/player.js
+++ src/js/modules/player.js
@@ -7,6 +7,8 @@ class Player {
         this._posX = px;
         this._posY = py;
         this._cellSize = cellSize;
+        this.health = 30;
+        this.maxHealth = 100;
     }
 
     update(x, y) {
@@ -14,7 +16,23 @@ class Player {
         this.ctx.beginPath();
         this.ctx.arc(x * this._cellSize + this._cellSize / 2, y * this._cellSize + this._cellSize / 2, 10, 0, 2 * Math.PI);
         this.ctx.fill();
+        this.drawHealthBar(x, y)
     }
+
+     drawHealthBar(x, y) {
+        let barHeight = 8;
+        let barWidth = 30
+        let barX = x * this._cellSize + (this._cellSize - barWidth) / 2;
+        let barY = y * this._cellSize - 5;
+    
+        let healthWidth = (this.health / this.maxHealth) * barWidth
+        this.ctx.fillStyle = "green";
+        this.ctx.fillRect(barX, barY, healthWidth, barHeight);
+    
+        this.ctx.strokeStyle = "black";
+        this.ctx.strokeRect(barX, barY, barWidth, barHeight);
+    }
+    
 
     clear(x, y) {
         const ctx = this.ctx;
